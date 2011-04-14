ef7ca22412bda31c41d9341d4c6d513efdb8ae7e
Christopher <metasoarous@gmail.com>
2011-04-14 14:19:58 -0700

    hopefully fixed the database connection issue (make the connection call specifically from the config hash, since this should have all of the right configurations now)

--- lib/casserver/server.rb
+++ lib/casserver/server.rb
@@ -256,7 +256,7 @@ module CASServer
 
     def self.init_database!
       #CASServer::Model::Base.establish_connection(config[:database])
-      ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || config[:database])
+      ActiveRecord::Base.establish_connection(config[:database])
       
       unless config[:disable_auto_migrations]
         print_cli_message "Running migrations to make sure your database schema is up to date..."
